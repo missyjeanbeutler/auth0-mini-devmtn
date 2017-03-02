4ad3a7c8208911b8dade9eb2b36341a6763be6ba
Missy Beutler <missyjeanbeutler@users.noreply.github.com>
2017-03-02 12:09:18 -0700

    further changes

--- index.js
+++ index.js
@@ -10,8 +10,10 @@ app.use(session({secret: config.sessionSecret}));
 app.use(passport.initialize());
 app.use(passport.session());
 
+
+
 passport.use(new Auth0Strategy({
-  domain: 'missyj.auth0.com',
+  domain: config.auth0.domain,
   clientID: config.auth0.id,
   clientSecret: config.auth0.secret,
   callbackURL: 'http://localhost:3000/auth/callback'
@@ -19,9 +21,24 @@ passport.use(new Auth0Strategy({
   return done(null, profile);
 }));
 
+app.get('/auth', passport.authenticate('auth0'));
+app.get('/auth/callback', passport.authenticate({successRedirect: '/', failureRedirect: '/auth'}))
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+app.get('/me', function(req, res){
+    res.send(req.user);
+})
 
 
 
-app.listening(3000, function(){
+
+app.listen(3000, function(){
     console.log('listening on port 3000')
 })
